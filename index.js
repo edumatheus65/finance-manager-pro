e576bfc8c00c7e17bd9090e0b9bd07673ba5bafd
edumatheus65 <ematheusneves13@gmail.com>
2024-08-28 09:10:13 -0300

    Renamed project folder to FinanceManagerPro

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import "dotenv/config.js";
-import express from "express";
+import express from "express"; // The express for default not supported Json syntax
 import { PostgresHelper } from "./src/db/postgres/helper.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", async (req, res) => {
+app.use(express.json());
+
+app.get("/api/users", async (req, res) => {
   const results = await PostgresHelper.query("SELECT * FROM users;");
 
   res.send(JSON.stringify(results));
+});
+
+app.post("/api/users", async (req, res) => {
+  console.log(req.body);
+  console.log(req.headers);
+  res.status(201).send("User created successfully");
 });
 
 app.listen(port, () => {
