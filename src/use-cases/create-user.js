516c682abd989e943bf4b061871d247051af1e03
edumatheus65 <ematheusneves13@gmail.com>
2024-09-13 11:03:14 -0300

    fix: ensure the password in the user is encrypted

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -6,7 +6,6 @@ import { EmailAlreadyInUseError } from "../erros/user.js";
 
 export class CreateUserUseCase {
   async execute(createUserParams) {
-    // TODO: verificar se o e-mail já está em uso
     const postgresGetUserByEmail = new PostgresGetUserByEmailRepository();
 
     const userWithProviderEmail = await postgresGetUserByEmail.execute(
@@ -17,20 +16,16 @@ export class CreateUserUseCase {
       throw new EmailAlreadyInUseError(createUserParams.email);
     }
 
-    // gerar id do usuário
     const userId = uuidv4();
 
-    // criptografar a senha
-    const hashedPassword = bcrypt.hash(createUserParams.password, 10);
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
 
-    // inserir o usuário no banco de dados
     const user = {
       ...createUserParams,
       id: userId,
       password: hashedPassword,
     };
 
-    // chamar o repositório
     const postgresCreateUserRepository = new PostgresCreateUserRepository();
 
     const createdUser = await postgresCreateUserRepository.execute(user);
