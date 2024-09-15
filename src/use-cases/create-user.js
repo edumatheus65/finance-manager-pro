e7ed769a9eb7ee36b47c48833848be4e383aeafd
edumatheus65 <ematheusneves13@gmail.com>
2024-09-15 17:50:07 -0300

    feat: add dependency injection to create user route

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,12 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
-import {
-  PostgresCreateUserRepository,
-  PostgresGetUserByEmailRepository,
-} from "../repositories/postgres/index.js";
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/index.js";
 import { EmailAlreadyInUseError } from "../erros/user.js";
 
 export class CreateUserUseCase {
+  constructor(postgresCreateUserRepository) {
+    this.postgresCreateUserRepository = postgresCreateUserRepository;
+  }
   async execute(createUserParams) {
     const postgresGetUserByEmail = new PostgresGetUserByEmailRepository();
 
@@ -28,9 +28,7 @@ export class CreateUserUseCase {
       password: hashedPassword,
     };
 
-    const postgresCreateUserRepository = new PostgresCreateUserRepository();
-
-    const createdUser = await postgresCreateUserRepository.execute(user);
+    const createdUser = await this.postgresCreateUserRepository.execute(user);
 
     return createdUser;
   }
