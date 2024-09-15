999fff0e31f7c4a30140ff94659d967aee2d5081
edumatheus65 <ematheusneves13@gmail.com>
2024-09-15 18:00:22 -0300

    feat: add dependency injection to get user by email repository

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,18 +1,17 @@
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
-import { PostgresGetUserByEmailRepository } from "../repositories/postgres/index.js";
 import { EmailAlreadyInUseError } from "../erros/user.js";
 
 export class CreateUserUseCase {
-  constructor(postgresCreateUserRepository) {
+  constructor(postgresCreateUserRepository, postgresGetUserByEmailRepository) {
     this.postgresCreateUserRepository = postgresCreateUserRepository;
+    this.postgresGetUserByEmailRepository = postgresGetUserByEmailRepository;
   }
   async execute(createUserParams) {
-    const postgresGetUserByEmail = new PostgresGetUserByEmailRepository();
-
-    const userWithProviderEmail = await postgresGetUserByEmail.execute(
-      createUserParams.email
-    );
+    const userWithProviderEmail =
+      await this.postgresGetUserByEmailRepository.execute(
+        createUserParams.email
+      );
 
     if (userWithProviderEmail) {
       throw new EmailAlreadyInUseError(createUserParams.email);
