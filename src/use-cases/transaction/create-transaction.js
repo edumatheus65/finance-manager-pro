39411172c902eb2a0a321b833e10ccce37ac3d20
edumatheus65 <ematheusneves13@gmail.com>
2024-09-18 11:41:30 -0300

    feat: add CreateTransactionUseCase

---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,28 @@
+import { UserNotFoundError } from "../../erros/user";
+import { v4 as uuidv4 } from "uuid";
+
+export class CreateTransactionUseCase {
+  constructor(getUserByIdRepository, createTransactionRepository) {
+    this.getUserByIdRepository = getUserByIdRepository;
+    this.createTransactionRepository = createTransactionRepository;
+  }
+
+  async execute(createTransactionParams) {
+    const userId = createTransactionParams.userId;
+
+    const user = await this.getUserByIdRepository.execute(userId);
+
+    if (!user) {
+      throw new UserNotFoundError(userId);
+    }
+
+    const transactionId = uuidv4();
+
+    const transaction = await this.createTransactionRepository({
+      ...createTransactionParams,
+      id: transactionId,
+    });
+
+    return transaction;
+  }
+}
