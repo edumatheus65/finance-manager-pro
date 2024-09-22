25948d0f0656011d4d3dc5ab406bb46d29ad6568
edumatheus65 <ematheusneves13@gmail.com>
2024-09-22 14:18:18 -0300

    feat: add route get transanction by user id

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import {
   makeGetUserByIdController,
   makeUpdateUserController,
 } from "./src/factories/controllers/user.js";
-import { makeCreateTransactionController } from "./src/factories/controllers/transaction.js";
+import {
+  makeCreateTransactionController,
+  makeGetTransactionByUserIdController,
+} from "./src/factories/controllers/transaction.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -49,6 +52,17 @@ app.post("/api/transaction", async (req, res) => {
   const createTransactionController = makeCreateTransactionController();
 
   const { statusCode, body } = await createTransactionController.execute(req);
+
+  res.status(statusCode).send(body);
+});
+
+app.get("/api/transaction", async (req, res) => {
+  const getTransactionByUserIdController =
+    makeGetTransactionByUserIdController();
+
+  const { statusCode, body } = await getTransactionByUserIdController.execute(
+    req
+  );
 
   res.status(statusCode).send(body);
 });
--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -1,9 +1,31 @@
 import { CreateTransactionController } from "../../controllers/index.js";
-import { CreateTransactionUseCase } from "../../use-cases/index.js";
+import {
+  CreateTransactionUseCase,
+  GetTransactionsByUserIdUseCase,
+} from "../../use-cases/index.js";
 import {
   PostgresGetUserByIdRepository,
   PostgresCreateTransactionRepository,
+  PostgresgetGetTransactionsByUserIdRepository,
 } from "../../repositories/postgres/index.js";
+import { GetTransactionsByUserIdController } from "../../controllers/transaction/get-transaction.js";
+
+export const makeGetTransactionByUserIdController = () => {
+  const postgresGetUserByIdRepository = new PostgresGetUserByIdRepository();
+
+  const postgresgetGetTransactionsByUserIdRepository =
+    new PostgresgetGetTransactionsByUserIdRepository();
+
+  const getTransactionsByUserIdUseCase = new GetTransactionsByUserIdUseCase(
+    postgresGetUserByIdRepository,
+    postgresgetGetTransactionsByUserIdRepository
+  );
+
+  const getTransactionByUserIdController =
+    new GetTransactionsByUserIdController(getTransactionsByUserIdUseCase);
+
+  return getTransactionByUserIdController;
+};
 
 export const makeCreateTransactionController = () => {
   const getUserByIdRepository = new PostgresGetUserByIdRepository();
