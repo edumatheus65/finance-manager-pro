0d1ca3de635c4c408f3d205eb42835bfec9743f4
edumatheus65 <ematheusneves13@gmail.com>
2024-09-23 11:22:06 -0300

    feat: add GetUserBalanceUseCase

---
+++ src/use-cases/user/get-user-balance.js
@@ -0,0 +1,20 @@
+import { UserNotFoundError } from "../../erros/user.js";
+
+export class GetUserBalanceUseCase {
+  constructor(postgresGetUserById, postgresGetUserBalanceRepository) {
+    this.postgresGetUserById = postgresGetUserById;
+    this.postgresGetUserBalanceRepository = postgresGetUserBalanceRepository;
+  }
+  async execute(params) {
+    const user = await this.postgresGetUserById.execute(params.userId);
+
+    if (!user) {
+      throw new UserNotFoundError();
+    }
+    const balance = await this.postgresGetUserBalanceRepository.execute(
+      params.userId
+    );
+
+    return balance[0];
+  }
+}
