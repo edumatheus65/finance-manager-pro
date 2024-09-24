2dc3f9a63e65ede320491209392c3e918ae8fd65
edumatheus65 <ematheusneves13@gmail.com>
2024-09-23 21:53:02 -0300

    fix: delete code line transaction_type

--- src/db/postgres/migrations/01-init.sql
+++ src/db/postgres/migrations/01-init.sql
@@ -6,16 +6,12 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(100) NOT NULL
 );
 
-DROP TYPE IF EXISTS transaction_type CASCADE;
-CREATE TYPE transaction_type AS ENUM ('EARNING', 'EXPENSE', 'INVESTMENT');
-
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'transaction_type') THEN
         CREATE TYPE transaction_type AS ENUM ('EARNING', 'EXPENSE', 'INVESTMENT');
     END IF;
 END $$;
-
 
 CREATE TABLE IF NOT EXISTS transactions (
     ID UUID PRIMARY KEY,
