7f584b81f7909197ab041f54b3ea724c86583246
edumatheus65 <ematheusneves13@gmail.com>
2024-09-28 14:08:57 -0300

    feat: ensure returned 400 if last_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -52,4 +52,23 @@ describe("Create User Controller", () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it("should return 400 if last_name is not provided", async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: "Edu",
+                email: "john.doe@example.com",
+                password: "password123"
+            }
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
